ad9dcdbbd6e42e224bdb74e190d221bbb68431f3
George Kelemouridis <geokele@gmail.com>
2021-04-03 13:20:05 +0100

    change

--- product/product.go
+++ product/product.go
@@ -1,7 +1,7 @@
 package product
 
 type Product struct {
-	ProductId      int    `json:"productId"`
+	ProductId      *int   `json:"productId"`
 	Manufacturer   string `json:"manufacturer"`
 	Sku            string `json:"sku"`
 	Upc            string `json:"upc"`
